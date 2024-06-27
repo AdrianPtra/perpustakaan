b83c64a58e1e37aa604ed03c0a1505dc5f8adf46
AdrianPtra <105776725+AdrianPtra@users.noreply.github.com>
2024-06-27 10:56:13 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,5 @@
+<p></p>
+<body>
+    <center><font size="+3" face="arial">Sistem Informasi Perpustakaan</font></center>
+    <center><font size="+3" face="arial">SMA N 1 Boja</font></center>
+</body>
